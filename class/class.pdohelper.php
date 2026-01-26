74bf2a5ec7a277a09fd556deff443aab810287e4
Lê Văn Phú <hackerpro536@gmail.com>
2026-01-26 08:56:25 +0700

    clean

--- class/class.pdohelper.php
+++ class/class.pdohelper.php
@@ -3,40 +3,8 @@
  * PdoHelper
  *
  * Pdo Helper for using PDO methods with Helper functions
- *
- * PHP version 5.3.13
- *
- * MIT License
- *
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
  * @category   PHP PDO Helper Class
  * @package    PdoHelper
- * @author     Neeraj Singh <neeraj.singh@lbi.co.in>
- * @author     Bhaskar Rabha <bhaskar.rabha@lbi.co.in>
- * @author     Priyadarshan Salkar <priyadarshan.salkar@lbi.co.in>
- * @copyright  2013-14 The PHP Group Of LBi India
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    1.0 Beta
  */
 /** Class Start **/
